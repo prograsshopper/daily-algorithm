a82f02a263cace38aa1bdd5050e7e2159f72018e
prograsshopper <tjalwled12@gmail.com>
2021-02-25 23:55:30 +0900

    Hash: 완주하지 못한 선수

--- programmers/20210225_hash.py
+++ programmers/20210225_hash.py
@@ -1,10 +1,11 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42576
+import unittest
 
-def solution(participant, completion):
+def solution(participant: list, completion: list) -> str:
     from collections import defaultdict
     from itertools import zip_longest
     
-    result_dict = defaultdict(lambda:0)
+    result_dict: dict = defaultdict(lambda:0)
     
     for person in zip_longest(participant, completion):
         result_dict[person[0]] += 1
@@ -13,3 +14,10 @@ def solution(participant, completion):
     for val in result_dict:
         if result_dict[val] == 1:
             return val
+
+
+if __name__ == "__main__":
+    participant: list = ['marina', 'josipa', 'nikola', 'vinko', 'filipa']
+    completion: list = ['josipa', 'filipa', 'marina', 'nikola']
+
+    print(solution(participant, completion) == 'vinko')
